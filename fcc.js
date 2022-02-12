b4932024191dce06b9f57689faa9da1914a93636
lui viton <artem_5511@mail.ru>
2022-02-12 15:56:19 +0300

    Update fcc.js
    fcc

--- fcc.js
+++ fcc.js
@@ -10,3 +10,25 @@ var a = 9
 //Объявить строковые переменные
 var myFirstName = "Artem";
 var myLastName = "Shukalovich";
+
+// Понимание неинициализированных переменных
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Понимание чувствительности к регистру в переменных
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
