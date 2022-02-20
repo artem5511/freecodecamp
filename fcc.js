cbb6a90106372d0c0e698e3a7d7e09278a80ba2f
lui viton <artem_5511@mail.ru>
2022-02-21 00:03:45 +0300

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -148,4 +148,15 @@ const lastLetterOfLastName = lastName[lastName.length - 1];
 const lastName = "Lovelace";
 
 // Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; 
+
+//Пустые слова
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "I today " + myVerb + " becouse " + myAdverb + " runs " + myNoun + " yes " + myAdjective + " ."; // Change this line
+// Only change code above this line
+console.log(wordBlanks)
