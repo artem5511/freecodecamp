335fdd163c2d91a09f55e45bac4221cf7e41646f
lui viton <artem_5511@mail.ru>
2022-02-21 22:56:43 +0300

    Update free2.js

---
+++ free2.js
@@ -0,0 +1,8 @@
+//Работа с массивами с помощью shift()
+const myArray = [["John", 23], ["dog", 3]];
+const removedFromMyArray = myArray.shift();
+
+//Работа с массивами с помощью unshift()
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
