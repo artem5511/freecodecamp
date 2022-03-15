8b31af9b376b9c40daea153a4c778baab9d734ff
lui viton <artem_5511@mail.ru>
2022-03-15 23:20:56 +0300

    Assignment with a Returned Value
    Assignment with a Returned Value

--- free2.js
+++ free2.js
@@ -69,3 +69,15 @@ function addFive() {
 
 addThree();
 addFive();
+
+//Присваивание с возвращаемым значением
+// Setup
+let processed = 2;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7)
+// Only change code below this line
+
