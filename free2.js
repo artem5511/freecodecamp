9968efd8191ef1abe57a2f4bf7e92b7642019490
lui viton <artem_5511@mail.ru>
2022-03-15 23:05:34 +0300

    Local Scope and Functions
    Local Scope and Functions

--- free2.js
+++ free2.js
@@ -25,4 +25,15 @@ oopsGlobal=5;
 function fun1(x) {
   x=5
 }
- //
+ 
+ //Локальный охват и функции
+ function myLocalScope() {
+  // Only change code below this line
+   const myVar = "";
+   console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
