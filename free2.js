71b973dfa5482294a3db866a58b9a56e5e96e46e
lui viton <artem_5511@mail.ru>
2022-03-17 00:16:20 +0300

    Stand in Line

--- free2.js
+++ free2.js
@@ -5,4 +5,20 @@ function reusableFunction() {
   reusableFunction();
   }
 
+  //Stand in Line
+  function nextInLine(arr, item) {
+    arr.push(item);
+    
+    var temp = arr.shift();
+    
+    return temp; // Change this line
+    }
+    
+    // Test Setup
+    var testArr = [1,2,3,4,5];
+    
+    // Display code
+    console.log("Before: " + JSON.stringify(testArr));
+    console.log(nextInLine(testArr, 6));
+    console.log("After: " + JSON.stringify(testArr));
   
