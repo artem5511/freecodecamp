edd481249c9bd858bad3782e94e2ecd3f0595c48
lui viton <artem_5511@mail.ru>
2022-03-20 22:27:06 +0300

    Comparison with the Equality Operator

--- free2.js
+++ free2.js
@@ -51,3 +51,18 @@ function trueOrFalse(wasThatTrue) {
 }
 
 trueOrFalse(false);
+
+//Comparison with the Equality Operator
+//Сравнение с оператором равенства
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+testEqual(12);
+testEqual("12");
