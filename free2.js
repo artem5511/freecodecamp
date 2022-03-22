52b69658b0a9ddfe53e1d2dd64216d6539eea2e8
lui viton <artem_5511@mail.ru>
2022-03-23 00:04:59 +0300

    Update free2.js
    Comparison with the Greater Than Operator

--- free2.js
+++ free2.js
@@ -116,3 +116,19 @@ function testStrictNotEqual(val) {
 }
 
 testStrictNotEqual(10);
+
+//Comparison with the Greater Than Operator
+//Сравнение с оператором больше, чем
+function testGreaterThan(val) {
+  if (val>100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val>10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
