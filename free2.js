c50e33b82aa265d085a0ddc91e3a566e622ab982
lui viton <artem_5511@mail.ru>
2022-03-23 23:46:23 +0300

    Update free2.js
    Сравнения с логическим оператором Or

--- free2.js
+++ free2.js
@@ -196,4 +196,16 @@ function testLogicalAnd(val) {
 
 testLogicalAnd(10);
 
-//
+//Сравнения с логическим оператором Or
+function testLogicalOr(val) {
+  Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
