e870529c2e8ff36267321371fcb3de58d675d08f
lui viton <artem_5511@mail.ru>
2022-03-27 00:01:57 +0300

    Update free2.js
    Golf Code

--- free2.js
+++ free2.js
@@ -253,8 +253,7 @@ orderMyLogic(7);
 //Цепочка операторов If Else
 function testSize(num) {
   // Only change code below this line
-if (num < 5) {
-  return "Tiny";}
+  
  else if (num < 10) {
   return "Small";}
   else if (num < 15) {
@@ -267,3 +266,30 @@ if (num < 5) {
   // Only change code above this line
 }
 testSize(7);
+
+//Golf Code
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  if (strokes<=1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par-2) {
+    return "Eagle";
+  } else if (strokes <= par-1) {
+    return "Birdie";
+   } else if (strokes <= par) {
+    return "Par";
+  }  else if (strokes <= par+1) {
+    return "Bogey";
+   } else if (strokes <= par+2) {
+    return "Double Bogey";
+  }  else if (strokes >= par+3) {
+    return "Go Home!";
+    } else {
+  // Only change code below this line
+  return "Change Me";
+  // Only change code above this line
+ }
+}
+
+golfScore(5, 4);
