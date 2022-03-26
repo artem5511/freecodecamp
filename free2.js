738cd4d73c7dfb878b98536f084911a871e82f41
lui viton <artem_5511@mail.ru>
2022-03-26 17:50:56 +0300

    Update free2.js
    Логический порядок в операторах If Else

--- free2.js
+++ free2.js
@@ -236,3 +236,16 @@ function testElseIf(val) {
   }
 }
 testElseIf(7);
+
+//Логический порядок в операторах If Else
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+orderMyLogic(7);
