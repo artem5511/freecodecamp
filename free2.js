30c28f63759f5ffb5cb7df4f3234667c465c8dfc
lui viton <artem_5511@mail.ru>
2022-03-26 17:43:47 +0300

    Update free2.js
     Else If

--- free2.js
+++ free2.js
@@ -225,4 +225,14 @@ function testElse(val) {
 }
 testElse(4);
 
-//
+//Знакомство с операторами Else If
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+  return "Between 5 and 10";
+  }
+}
+testElseIf(7);
